68ccd793d53e0d100e62add7d7ed36df86943f84
noahvsb <noahvansteenbrugge@gmail.com>
2024-11-30 11:20:05 +0100

    ran and processed benchmarks

--- benchmark/BenchmarkRainbowVSRedBlack.java
+++ benchmark/BenchmarkRainbowVSRedBlack.java
@@ -2,25 +2,23 @@ import helpfunctions.BenchmarkHelpFunctions;
 
 public class BenchmarkRainbowVSRedBlack {
     public static void main(String[] args) {
-        String filePath = "benchmark/benchmark_rainbow_vs_redblack.csv";
+        String filePath = "benchmark/result/benchmark_rainbow_vs_redblack.csv";
         BenchmarkHelpFunctions.clearFile(filePath);
         BenchmarkHelpFunctions.writeToFile(filePath, "tree;k;n;operation;time");
 
         // RED BLACK TREE
-        BenchmarkHelpFunctions.redBlackTree(100, 10, filePath);
-        BenchmarkHelpFunctions.redBlackTree(100, 100, filePath);
         BenchmarkHelpFunctions.redBlackTree(100, 1000, filePath);
         BenchmarkHelpFunctions.redBlackTree(100, 10000, filePath);
-        BenchmarkHelpFunctions.redBlackTree(10, 100000, filePath);
-        BenchmarkHelpFunctions.redBlackTree(5, 1000000, filePath);
+        BenchmarkHelpFunctions.redBlackTree(100, 100000, filePath);
+        BenchmarkHelpFunctions.redBlackTree(100, 500000, filePath);
+        BenchmarkHelpFunctions.redBlackTree(100, 1000000, filePath);
 
         // RAINBOW TREE WITH MOST EFFICIENT K
-        int k = 3;
-        BenchmarkHelpFunctions.rainbowTree(k, 100, 10, filePath);
-        BenchmarkHelpFunctions.rainbowTree(k, 100, 100, filePath);
+        int k = 9;
         BenchmarkHelpFunctions.rainbowTree(k, 100, 1000, filePath);
         BenchmarkHelpFunctions.rainbowTree(k, 100, 10000, filePath);
-        BenchmarkHelpFunctions.rainbowTree(k, 10, 100000, filePath);
-        BenchmarkHelpFunctions.rainbowTree(k, 5, 1000000, filePath);
+        BenchmarkHelpFunctions.rainbowTree(k, 100, 100000, filePath);
+        BenchmarkHelpFunctions.rainbowTree(k, 100, 500000, filePath);
+        BenchmarkHelpFunctions.rainbowTree(k, 100, 1000000, filePath);
     }
 }
